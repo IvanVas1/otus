ce726dc50f300339e8c98d247406b5f78d08f53c
Иван Васильев <ivanvasilev@MacBook-Pro-Ivan-8.local>
2024-11-12 19:30:43 +0300

    fix tests

--- feedback-tests/likes/profile-likes.test.js
+++ feedback-tests/likes/profile-likes.test.js
@@ -19,7 +19,6 @@ describe('Проверка эндпоинта GET profile/likes', () => {
     expect(response.body).toHaveProperty('type', expect.any(String))
     expect(response.body).toHaveProperty('hash', expect.any(String))
     expect(response.body).toHaveProperty('totalItems', expect.any(Number))
-    expect(response.body).toHaveProperty('lastModifiedAt', expect.any(String))
     expect(response.body).toHaveProperty('response', expect.any(Array))
   })
 })
--- feedback-tests/wishlist/profile-wishlist.test.js
+++ feedback-tests/wishlist/profile-wishlist.test.js
@@ -20,7 +20,6 @@ describe('Проверка эндпоинта GET profile/wishlist', () => {
     expect(response.body).toHaveProperty('type', expect.any(String))
     expect(response.body).toHaveProperty('hash', expect.any(String))
     expect(response.body).toHaveProperty('totalItems', expect.any(Number))
-    expect(response.body).toHaveProperty('lastModifiedAt', expect.any(String))
     expect(response.body).toHaveProperty('response', expect.any(Array))
   })
 })
